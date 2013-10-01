0ba6ece9579a98a019155e782d8ba509f0eba3ce
piotrekbass <piotrek.koscielniak@gmail.com>
2013-10-01 10:38:56 +0200

    ajaxService

--- public/js/controllers/postsController.js
+++ public/js/controllers/postsController.js
@@ -1,12 +1,11 @@
 /*global app*/
 (function () {
     "use strict";
-    app.controller('PostsController', ['$scope', 'postsService',
-        function ($scope, postsService) {
+    app.controller('PostsController', ['$scope', 'ajaxService',
+        function ($scope, ajaxService) {
 
             function fetchPosts() {
-                postsService.getPosts().then(function (data) {
-                    debugger;
+                ajaxService.doGet('posts').then(function (data) {
                     $scope.posts = data;
                 });
             }
--- public/js/services/ajaxService.js
+++ public/js/services/ajaxService.js
@@ -1,27 +1,20 @@
 ﻿/* global appServices*/
-appServices.factory('ajaxService', ['$http', '$q', 'notifyService', 'uiBlocker', 'dateTimeUtils',
-    function ($http, $q, notifyService, uiBlocker, dateTimeUtils) {
+appServices.factory('ajaxService', ['$http', '$q', 'notifyService',
+    function ($http, $q, notifyService) {
         "use strict";
 
         var errorMessage = "Wystąpił błąd podczas przetwarzania żądania";
 
-        function doPost(url, dataToSend, shouldBlockUi) {
-            blockUi(shouldBlockUi);
+        function doPost(url, dataToSend) {
             var deferred = $q.defer();
             $http.post(url, dataToSend)
                 .success(function (data) {
-                    unblockUi(blockUi);
                     deferred.resolve(data);
                 })
                 .error(function (data, status) {
-                    unblockUi(blockUi);
                     handleRequestFail(deferred, status);
                 });
             return deferred.promise;
-        }
-
-        function getTimestamp() {
-            return dateTimeUtils.getNow().getTime();
         }
 
         function getConfig(dataToSend) {
@@ -31,46 +24,22 @@ appServices.factory('ajaxService', ['$http', '$q', 'notifyService', 'uiBlocker',
             if (dataToSend) {
                 config.params = dataToSend;
             }
-            config.params._ = getTimestamp();
             return config;
         }
 
-        function doGet(url, dataToSend, shouldBlockUi) {
-            blockUi(shouldBlockUi);
+        function doGet(url, dataToSend) {
             var deferred = $q.defer();
 
             var config = getConfig(dataToSend);
 
             $http.get(url, config)
                 .success(function (data) {
-                    unblockUi(blockUi);
                     deferred.resolve(data);
                 })
                 .error(function (data, status) {
-                    unblockUi(blockUi);
                     handleRequestFail(deferred, status);
                 });
             return deferred.promise;
-        }
-
-        function doPostAndBlockUi(url, dataToSend) {
-            return doPost(url, dataToSend, true);
-        }
-
-        function doGetAndBlockUi(url, dataToSend) {
-            return doGet(url, dataToSend, true);
-        }
-
-        function blockUi(shouldBlock) {
-            if (shouldBlock) {
-                uiBlocker.block();
-            }
-        }
-
-        function unblockUi(blockUi) {
-            if (blockUi) {
-                uiBlocker.unblock();
-            }
         }
 
         function handleRequestFail(deferred, status) {
@@ -80,8 +49,6 @@ appServices.factory('ajaxService', ['$http', '$q', 'notifyService', 'uiBlocker',
 
         return {
             doGet: doGet,
-            doPost: doPost,
-            doPostAndBlockUi: doPostAndBlockUi,
-            doGetAndBlockUi: doGetAndBlockUi
+            doPost: doPost
         };
     }]);
